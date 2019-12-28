e27952f9c0beb4423f1a604a1673723897fdcbbf
KaminskiPiotr1984 <kaminskipiotr1984@gmail.com>
2019-12-28 21:21:03 +0100

    HomeControler - put adnotation @Slf4j.

--- src/main/java/pl/coderslab/charity/aplication/controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/aplication/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package pl.coderslab.charity.aplication.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@Slf4j
 public class HomeController {
 
 
