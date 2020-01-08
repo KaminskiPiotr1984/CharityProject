ca0eafaa611d7c04f98d72930add285d1864a377
KaminskiPiotr1984 <kaminskipiotr1984@gmail.com>
2020-01-08 18:41:41 +0100

    Create DonationController. The role of the controller will be to display and operate the form for adding gifts.

--- src/main/java/pl/coderslab/charity/aplication/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/aplication/controllers/DonationController.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity.aplication.controllers;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,33 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.aplication.domain.repositories.DonationsRepository;
 import pl.coderslab.charity.aplication.domain.repositories.InstitutionsRepository;
 
-
 @Controller
 @Slf4j
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/form")
 
+public class DonationController {
 
     private final InstitutionsRepository institutionsRepository;
     private final DonationsRepository donationsRepository;
 
-    public HomeController(InstitutionsRepository institutionsRepository, DonationsRepository donationsRepository) {
+    public DonationController(InstitutionsRepository institutionsRepository, DonationsRepository donationsRepository) {
         this.institutionsRepository = institutionsRepository;
         this.donationsRepository = donationsRepository;
     }
 
 
-
-
-
     @GetMapping
-    public String homeAction(Model model) {
+    public String formAction(Model model) {
         model.addAttribute("institutions", institutionsRepository.findAll());
         model.addAttribute("donations", donationsRepository.findAll());
 
-        return "index";
+        return "form";
     }
-
-
 
 }
