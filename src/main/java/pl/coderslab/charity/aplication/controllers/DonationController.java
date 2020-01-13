bc4eab4241a2c7f20d7bb0abc59d4d18fb470eb5
KaminskiPiotr1984 <kaminskipiotr1984@gmail.com>
2020-01-13 18:19:02 +0100

    DonationController - Supplementing the code with support for receiving data from the form.

--- src/main/java/pl/coderslab/charity/aplication/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/aplication/controllers/DonationController.java
@@ -4,10 +4,16 @@ package pl.coderslab.charity.aplication.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.aplication.domain.entities.Donation;
+import pl.coderslab.charity.aplication.domain.repositories.CategoryRepository;
 import pl.coderslab.charity.aplication.domain.repositories.DonationsRepository;
 import pl.coderslab.charity.aplication.domain.repositories.InstitutionsRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -17,19 +23,31 @@ public class DonationController {
 
     private final InstitutionsRepository institutionsRepository;
     private final DonationsRepository donationsRepository;
+    private final CategoryRepository categoryRepository;
 
-    public DonationController(InstitutionsRepository institutionsRepository, DonationsRepository donationsRepository) {
+    public DonationController(InstitutionsRepository institutionsRepository, DonationsRepository donationsRepository, CategoryRepository categoryRepository) {
         this.institutionsRepository = institutionsRepository;
         this.donationsRepository = donationsRepository;
+        this.categoryRepository = categoryRepository;
     }
 
 
     @GetMapping
     public String formAction(Model model) {
         model.addAttribute("institutions", institutionsRepository.findAll());
-        model.addAttribute("donations", donationsRepository.findAll());
-
+        model.addAttribute("category", categoryRepository.findAll());
+        model.addAttribute("donation", new Donation());
         return "form";
     }
+
+    @PostMapping()
+    public String formFilling(@Valid Donation donation, BindingResult result) {
+        if (result.hasErrors()) {
+            return "form";
+        }
+        donationsRepository.save(donation);
+        return "/";
+    }
+
 
 }
